c36c3eb1761a1ab7b99367681c703d58875ae045
Dalton Baird <dltnbaird@gmail.com>
2016-03-09 02:03:05 -0800

    Added command instances with descriptions.

--- WNSChat.Common/Commands/Command.cs
+++ WNSChat.Common/Commands/Command.cs
@@ -18,21 +18,13 @@ namespace WNSChat.Common.Commands
         public PermissionLevel PermissionLevel { get; }
 
         /// <summary>
-        /// Called when the command is executed.  CommandExceptions thrown here will be caught
-        /// by the system.  This will only be called if the user has the appropriate permissions.
-        /// </summary>
-        /// <param name="user">The command user</param>
-        /// <param name="restOfLine">The rest of the line that the command was entered on</param>
-        public Action<IUser, string> Execute { get; }
-
-        /// <summary>
         /// Constructs a new Command
         /// </summary>
         /// <param name="name">The name of the command, what you have to type in</param>
         /// <param name="description">The description of the command</param>
         /// <param name="usage">A string describing how to use the command</param>
         /// <param name="permissionLevel">The minimum permission level required to run the command</param>
-        public Command(string name, string description, string usage, PermissionLevel permissionLevel, Action<IUser, string> executeDelegate)
+        public Command(string name, string description, string usage, PermissionLevel permissionLevel)
         {
             if (name.Contains(" "))
                 throw new ArgumentException("Invalid command name, contains a space.", nameof(name));
@@ -41,16 +33,21 @@ namespace WNSChat.Common.Commands
             this.Description = description;
             this.Usage = usage;
             this.PermissionLevel = permissionLevel;
-            this.Execute = executeDelegate;
         }
 
-        ///// <summary>
-        ///// Called when the command is executed.  CommandExceptions thrown here will be caught
-        ///// by the system.  This will only be called if the user has the appropriate permissions.
-        ///// </summary>
-        ///// <param name="user">The command user</param>
-        ///// <param name="restOfLine">The rest of the line that the command was entered on</param>
-        //public abstract void Execute(IUser user, string restOfLine);
+        /// <summary>
+        /// Called when the command is executed.  CommandExceptions thrown here will be caught
+        /// by the system.
+        /// </summary>
+        /// <param name="user">The command user</param>
+        /// <param name="restOfLine">The rest of the line that the command was entered on</param>
+        public void OnExecute(IUser user, string restOfLine)
+        {
+            if (this.CanUserExecuteCommand(user))
+                this.Execute?.Invoke(user, restOfLine);
+            else
+                throw new CommandException($"You do not have permission to use this command! Your permission level: {user.PermissionLevel}, Required: {this.PermissionLevel}");
+        }
 
         /// <summary>
         /// Finds out if the user can use this command
@@ -76,5 +73,13 @@ namespace WNSChat.Common.Commands
         {
             return this.Name;
         }
+
+        /// <summary>
+        /// Fired when the command is executed.  CommandExceptions thrown here will be caught
+        /// by the system.  This will only be fired if the user has the appropriate permissions.
+        /// </summary>
+        /// <param name="user">The command user</param>
+        /// <param name="restOfLine">The rest of the line that the command was entered on</param>
+        public event Action<IUser, string> Execute;
     }
 }
